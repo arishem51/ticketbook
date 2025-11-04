0a552d4f3855a71db22d62959c21be98cdefce80
arishem <arishem51@gmail.com>
2025-11-04 08:46:33 +0700

    first commit

---
+++ ticketbook/src/test/java/com/swd/ticketbook/TicketbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd.ticketbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
